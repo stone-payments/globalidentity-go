a67efd4c3f65a5e7f00edb887a4012b2384a19b7
João Azevedo | Stone <jazevedo@stone.com.br>
2017-06-14 23:37:32 -0300

    RC-1809 Valida response

--- response.go
+++ response.go
@@ -1,6 +1,16 @@
 package globalidentity
 
+// Response is the base response of Global Identity.
 type Response struct {
 	Success         bool     `json:"Success"`
 	OperationReport []string `json:"OperationReport"`
+}
+
+// Validate checks success of response.
+func (r *Response) Validate() error {
+	if r.Success != true {
+		return GlobalIdentityError(r.OperationReport)
+	}
+
+	return nil
 }
