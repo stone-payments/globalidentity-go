d5dc203300e25c720a4180fdd88eee861347cf57
João Azevedo | Stone <jazevedo@stone.com.br>
2017-08-09 15:55:24 -0300

    RC-2058 Testa metodo que retorna usuario

--- management/manager_test.go
+++ management/manager_test.go
@@ -4,21 +4,23 @@ import (
 	"net/http"
 	"testing"
 
+	"github.com/fortytw2/leaktest"
 	"github.com/jarcoal/httpmock"
 	core "github.com/stone-payments/globalidentity-go"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/suite"
-	"github.com/fortytw2/leaktest"
 )
 
 type ManagementSuite struct {
 	suite.Suite
 	userRolesUrl         string
+	getUserUrl           string
 	listUsersUrl         string
 	manager              GlobalIdentityManager
 	errorResponder       httpmock.Responder
 	okUserRolesResponder httpmock.Responder
 	okListUsersResponder httpmock.Responder
+	okGetUserResponder   httpmock.Responder
 	failedResponder      httpmock.Responder
 	wrongResponder       httpmock.Responder
 }
@@ -30,9 +32,11 @@ func TestManager(t *testing.T) {
 func (suite *ManagementSuite) SetupTest() {
 	suite.manager = New("key", "key", "http://userRolesUrl")
 	suite.userRolesUrl = "http://userRolesUrl/api/management/key/users/user/roles"
+	suite.getUserUrl = "http://userRolesUrl/api/management/key/users/email?includeRoles=true"
 	suite.listUsersUrl = "http://userRolesUrl/api/management/key/users?page=1&limit=1&includeRoles=true"
 
 	suite.okUserRolesResponder = httpmock.NewStringResponder(http.StatusOK, `{"Success": true, "OperationReport": [], "roles":[{"statusName":"mock"}]}`)
+	suite.okGetUserResponder = httpmock.NewStringResponder(http.StatusOK, `{ "OperationReport": [], "Success": true, "user": { "active": true, "comment": "Comments about the user in the application", "email": "user1@email.com", "lockedOut": false, "name": "User's name", "roles": [ "ADMIN" ], "userKey": "00000000-0000-0000-0000-000000000000" } }`)
 	suite.okListUsersResponder = httpmock.NewStringResponder(http.StatusOK, `{ "OperationReport": [], "Success": true, "users": [ { "active": true, "comment": "Comments about the user in the application", "email": "user1@email.com", "lockedOut": false, "name": "User's name", "roles": [ "ADMIN" ], "userKey": "00000000-0000-0000-0000-000000000000" } ] }`)
 	suite.errorResponder = httpmock.NewStringResponder(http.StatusInternalServerError, "")
 	suite.wrongResponder = httpmock.NewStringResponder(http.StatusOK, "mock")
@@ -43,7 +47,6 @@ func (suite *ManagementSuite) TestUserRolesOk() {
 	defer leaktest.Check(suite.T())()
 	httpmock.Activate()
 	defer httpmock.DeactivateAndReset()
-
 
 	httpmock.RegisterResponder("GET", suite.userRolesUrl, suite.okUserRolesResponder)
 
@@ -149,5 +152,61 @@ func (suite *ManagementSuite) TestListUsersFailedResponse() {
 	_, ok := err.(core.GlobalIdentityError)
 
 	assert.Nil(suite.T(), users)
+	assert.True(suite.T(), ok)
+}
+
+func (suite *ManagementSuite) TestUserOk() {
+	defer leaktest.Check(suite.T())()
+	httpmock.Activate()
+	defer httpmock.DeactivateAndReset()
+
+	httpmock.RegisterResponder("GET", suite.getUserUrl, suite.okListUsersResponder)
+
+	user, err := suite.manager.User("email", true)
+
+	assert.NotNil(suite.T(), user)
+	assert.Nil(suite.T(), err)
+}
+
+func (suite *ManagementSuite) TestUserWrongResponse() {
+	defer leaktest.Check(suite.T())()
+	httpmock.Activate()
+	defer httpmock.DeactivateAndReset()
+
+	httpmock.RegisterResponder("GET", suite.getUserUrl, suite.wrongResponder)
+
+	user, err := suite.manager.User("email", true)
+
+	assert.Nil(suite.T(), user)
+	assert.NotNil(suite.T(), err)
+}
+
+func (suite *ManagementSuite) TestUserErrorResponse() {
+	defer leaktest.Check(suite.T())()
+	httpmock.Activate()
+	defer httpmock.DeactivateAndReset()
+
+	httpmock.RegisterResponder("GET", suite.getUserUrl, suite.errorResponder)
+
+	user, err := suite.manager.User("email", true)
+
+	_, ok := err.(core.GlobalIdentityError)
+
+	assert.Nil(suite.T(), user)
+	assert.True(suite.T(), ok)
+}
+
+func (suite *ManagementSuite) TestUserFailedResponse() {
+	defer leaktest.Check(suite.T())()
+	httpmock.Activate()
+	defer httpmock.DeactivateAndReset()
+
+	httpmock.RegisterResponder("GET", suite.getUserUrl, suite.failedResponder)
+
+	user, err := suite.manager.User("email", true)
+
+	_, ok := err.(core.GlobalIdentityError)
+
+	assert.Nil(suite.T(), user)
 	assert.True(suite.T(), ok)
 }
