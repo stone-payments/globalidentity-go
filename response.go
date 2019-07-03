3f36cf996598d5cc99d0c9a5a0d0d28682fd4f28
Pedro Fadel <pfadel@stone.com.br>
2019-07-03 19:37:17 -0300

    :bug: fix OperationReport struct mapping

--- response.go
+++ response.go
@@ -2,14 +2,22 @@ package globalidentity
 
 // Response is the base response of Global Identity.
 type Response struct {
-	Success         bool     `json:"Success"`
-	OperationReport []string `json:"OperationReport,omitempty"`
+	Success         bool `json:"Success"`
+	OperationReport []struct {
+		Field     string `json:"Field"`
+		Message   string `json:"Message"`
+		ErrorCode int    `json:"ErrorCode"`
+	} `json:"OperationReport,omitempty"`
 }
 
 // Validate checks success of response.
 func (r *Response) Validate() error {
 	if r.Success != true {
-		return GlobalIdentityError(r.OperationReport)
+		el := make([]string, 0)
+		for _, e := range r.OperationReport {
+			el = append(el, e.Message)
+		}
+		return GlobalIdentityError(el)
 	}
 
 	return nil
